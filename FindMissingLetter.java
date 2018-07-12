07bc8ff02469f89a945f1d33544428d60ed68d5f
Yiu Chung Yau (Tommy) <wwkb1233@gmail.com>
2018-07-11 23:08:05 -0400

    add FindMissingLetter.java

---
+++ FindMissingLetter.java
@@ -0,0 +1,25 @@
+/*
+Write a method that takes an array of consecutive (increasing) letters as input and that returns the missing letter in the array.
+
+You will always get an valid array. And it will be always exactly one letter be missing. The length of the array will always be at least 2.
+The array will always contain letters in only one case.
+
+Example:
+
+['a','b','c','d','f'] -> 'e'
+['O','Q','R','S'] -> 'P'
+(Use the English alphabet with 26 letters!)
+*/
+
+public class FindMissingLetter
+{
+  public static char findMissingLetter(char[] array)
+  {
+    for(int i = 0;i<array.length-1;i++){
+      if(array[i+1]-array[i]>1){
+        return (char) (array[i]+1);
+      }
+    }
+    return ' ';
+  }
+}

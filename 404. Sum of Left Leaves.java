8a1aa4418d097eaf645770d2c06b019e08a54c69
Yiu Chung Yau (Tommy) <wwkb1233@gmail.com>
2019-02-19 23:22:11 -0500

    Create 404. Sum of Left Leaves.java

---
+++ 404. Sum of Left Leaves.java
@@ -0,0 +1,21 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) { val = x; }
+ * }
+ */
+class Solution {
+    public int sumOfLeftLeaves(TreeNode root) {
+        if(root == null) return 0;
+        return sumOfLeftLeaves(root.left, true) + sumOfLeftLeaves(root.right, false);
+    }
+    
+    public int sumOfLeftLeaves(TreeNode node, boolean isLeft){
+        if(node == null) return 0;
+        if(node.left == null && node.right == null && isLeft) return node.val;
+        return sumOfLeftLeaves(node.left, true) + sumOfLeftLeaves(node.right, false);
+    }
+}

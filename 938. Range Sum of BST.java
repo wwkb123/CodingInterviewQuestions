50a0c5d97c42a7da71cb1fbcfd256fabd499716a
Yiu Chung Yau (Tommy) <wwkb1233@gmail.com>
2019-03-25 01:32:25 -0400

    Create 938. Range Sum of BST.java

---
+++ 938. Range Sum of BST.java
@@ -0,0 +1,16 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) { val = x; }
+ * }
+ */
+class Solution {
+    public int rangeSumBST(TreeNode root, int L, int R) {
+        if(root == null) return 0;
+        
+        return rangeSumBST(root.left, L, R) + rangeSumBST(root.right, L, R) + ((root.val >= L && root.val <= R) ? root.val : 0);
+    }
+}
